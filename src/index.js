e60ab55bd3adcd425690ac0f0da2b2a0bfb5ed0e
mrigank-newton <mrigank7903710344@gmail.com>
2021-06-15 19:10:45 +0530

    calculator-redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./App";
+import reducer from "./reducer/calculatorReducer";
+
+// npm install redux
+// npm install react-redux
+// create your reducer function
+// create your store using createStore(reducer fn)
+// pass the store to all the components using Provider component in react-redux
+//mapStateToProps in components which need any value in store
+//mapDispatchToProps in components which need to dispatch an action
+
+const store = createStore(reducer);
+console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
